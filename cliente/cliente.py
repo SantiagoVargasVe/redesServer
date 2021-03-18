8b9facb6d2875a1645d1fb18dcb1b9f3d7c4f5a0
Santiago Vargas Vega <santiagovargasve@gmail.com>
2021-03-18 12:28:00 -0500

    Se esta haciendo los logs del cliente, falta juntar los logs del cliente y del server

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -3,35 +3,37 @@ import os
 from hashlib import sha256
 from threading import Thread
 import math
-
+from time import perf_counter_ns
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-BUFFER_SIZE = 10420
-SEPARATOR = "<SEPARATOR>"
 integrity=''
 
-def createClients():
-
+def createClients(numConexiones):
+    namelog = ''
+    performance = ''
+    filename= ''
+    filesize = ''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        tic = perf_counter_ns()
         s.connect((HOST, PORT))
         s.sendall(b'Hola')
-        # filename = s.recv(BUFFER_SIZE)
-        # integrity = s.recv(BUFFER_SIZE).decode()
-        # filesize = s.recv(BUFFER_SIZE).decode()
         msg1 = s.recv(1024).decode()
         print(msg1)
         msg1= msg1.split('<>')
+        check_integrity=''
         security = ''
         integrity=msg1[1]
         filename= msg1[0]
+        extension= filename[filename.index('.'):]
+        namelog = filename[:filename.index('.')]
         filesize= int(msg1[2])
         num_iterations= math.ceil(filesize/4096)
         s.sendall('RECV_DATA'.encode())
-        with open(filename,'wb') as f:
+        with open(f'archivosRecibidos/{namelog}-Prueba-{numConexiones}{extension}','wb') as f:
             for i in range(num_iterations):
                 bytes_read = s.recv(4096)
                 f.write(bytes_read)
-        with open(filename,'rb') as j:
+        with open(f'archivosRecibidos/{namelog}-Prueba-{numConexiones}{extension}','rb') as j:
             sha = j.read()
             security= sha256(sha).hexdigest()
         
@@ -41,13 +43,23 @@ def createClients():
         print ('Voy a mandar mensaje')
         if (security == integrity):
             s.sendall(b'SUCCESS')
+            check_integrity = True
             print('Somos iguales')    
         else:
             s.sendall(b'ERROR')
-                
+            check_integrity = False
+        toc = perf_counter_ns()
+        performance = toc - tic 
+    with open(f'{namelog}.txt','w+') as n:
+        n.write(f'nombre_archivo:{filename}\n')
+        n.write(f'tamaño_archivo:{filesize}')
+        n.write(f'nombre_cliente:{namelog}\n')
+        n.write(f'Entrega_exitosa:{check_integrity}\n')
+        n.write(f'Tiempo:{performance}')
+
         
 
 number_clients = int(input("Cuantos clientes"))
 
 for i in range(0,number_clients):
-    Thread(target=createClients).start()
+    Thread(target=createClients, args=(number_clients,)).start()
---
+++ server.py
@@ -0,0 +1,32 @@
+import socket 
+from threading import Thread, Barrier
+import os
+from hashlib import sha256
+from newClient import NewClient
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '0.0.0.0'
+port = 65432
+i= 0
+numClients = int(input('Cuantos clientes simultaneos? \n'))
+# filename = 'big.txt' if filesend ==1 else 'file.txt'
+filename = input ('Escriba el nombre del archivo que sea enviar \n Ejemplo: prueba.txt \n')
+extension= filename[filename.index('.'):]
+filesize= os.path.getsize(filename)
+print('Server on')
+print ('Waiting for clients')
+s.bind((host,port))
+s.listen(25)
+b = Barrier(numClients)
+pruebas = []
+try:
+    while True:
+        c,addr = s.accept()
+        if b.broken:
+            b.reset()
+        name= f'client{i}{extension}'
+        i+=1
+        prueba =NewClient(c,addr,filename,name,filesize,b).start()
+        pruebas.append(prueba)
+except Exception as e:
+    print(e)
+    s.close()
